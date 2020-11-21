5fb518ec8f9509a07205376a504e1b59973bc0ac
mnkywlkr <patricia.putri.pd@gmail.com>
2020-11-21 23:40:09 +0100

    add product core service

---
+++ product-core-service/src/main/java/de/hska/vsmlab/product/CategoryController.java
@@ -0,0 +1,28 @@
+package de.hska.vsmlab.product;
+
+import de.hska.vsmlab.product.model.Category;
+import de.hska.vsmlab.product.model.CategoryRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class CategoryController implements ICategoryController {
+    @SuppressWarnings("SpringJavaAutowiredFieldsWarningInspection")
+    @Autowired
+    @Lazy
+    private CategoryRepo categoryRepo;
+
+    @Override
+    public ResponseEntity<Category> getCategory(final Long categoryId) {
+        final Optional<Category> category = categoryRepo.findById(categoryId);
+        if (category.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(category.get(), HttpStatus.OK);
+    }
+}
---
+++ product-core-service/src/main/java/de/hska/vsmlab/product/model/ProductRepo.java
@@ -0,0 +1,5 @@
+package de.hska.vsmlab.product.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepo extends CrudRepository<Product, Long> { }
