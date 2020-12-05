947aa0c74d6b73f80b4498ef795889d4f6a0a7a7
Armin Kunkel <armin@avensio.de>
2020-12-05 14:58:20 +0100

    Product Core Service entfernt

--- product-core-service/src/main/java/de/hska/vsmlab/microservice/product/model/CategoryRepo.java
+++
@@ -1,6 +0,0 @@
-package de.hska.vsmlab.microservice.product.model;
-
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CategoryRepo extends CrudRepository<Category, Long> { }
--- product-core-service/src/main/java/de/hska/vsmlab/microservice/product/model/ProductRepo.java
+++
@@ -1,5 +0,0 @@
-package de.hska.vsmlab.microservice.product.model;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepo extends CrudRepository<Product, Long> { }
