459e79be6821fe946371aee35738d15c41fe22cd
Armin Kunkel <armin@avensio.de>
2021-01-17 13:52:37 +0100

    Ressource Server Config für Role, Product, Core und Category

---
+++ category-core-service/src/main/java/de/hska/vsmlab/category/config/ResourceServerConfig.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.hska.vsmlab.category.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+@SuppressWarnings("deprecation")
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+	private static final String RESOURCE_ID = "category-core-service";
+
+	@Autowired
+	private TokenStore tokenStore;
+
+	@Override
+	public void configure(ResourceServerSecurityConfigurer security) throws Exception {
+		security
+			.resourceId(RESOURCE_ID)
+			.tokenStore(this.tokenStore);
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http
+			.authorizeRequests()
+				.antMatchers(HttpMethod.GET, "/category").access("#oauth2.hasScope('category.read')")
+                .antMatchers(HttpMethod.POST, "/category").access("#oauth2.hasScope('category.write')")
+                .antMatchers(HttpMethod.DELETE, "/category/*").access("#oauth2.hasScope('category.write')")
+            .anyRequest().authenticated();
+		// @formatter:on
+	}
+}
